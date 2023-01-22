76e2e79687cf371e08d1d16d212fd55739da5a82
rafacmasson <122946043+rafacmasson@users.noreply.github.com>
2023-01-21 21:48:42 -0300

    Arithmetic Operators solution added

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    summ = a + b
+    dif = a - b
+    prod = a * b
+    
+    print(summ)
+    print(dif)
+    print(prod)
